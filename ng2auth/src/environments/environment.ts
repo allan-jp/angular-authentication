1e3814eb276f0aabd853d6c6cc92b5c389edaeba
allan-jp <allan-jp@users.noreply.github.com>
2018-04-06 23:47:52 -0500

    Create environment.ts

---
+++ ng2auth/src/environments/environment.ts
@@ -0,0 +1,17 @@
+// Pulled from https://github.com/auth0-blog/angular-2-authentication-tutorial/blob/master/ng2auth/src/environments/environment.ts.example
+
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  auth: {
+    clientID: 'YOUR-AUTH0-CLIENT-ID',
+    domain: 'YOUR-AUTH0-DOMAIN', // e.g., you.auth0.com
+    audience: 'YOUR-AUTH0-API-IDENTIFIER', // e.g., http://localhost:3001
+    redirect: 'http://localhost:4200/callback',
+    scope: 'openid profile email'
+  }
+};
